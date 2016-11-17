c798f28f70c5e5ea686a34adae3ae7cb5a94ade0
Dr. Waldijk <DokterW@users.noreply.github.com>
2016-11-17 20:19:12 +0100

    Fix

--- start.sh
+++ start.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-# SHatomATOR v0.5
+# SHatomATOR v0.6
 # Made by Dr. Waldijk
 # Installs & Upgrades Atom Editor on Fedora.
 # Read the README.md for more info.
 # By running this script you agree to the license terms.
 # -----------------------------------------------------------------------------------
 echo "Checking..."
+ATOMRPM="atom.x86_64.rpm"
+ATOMDOWNLOAD="https://github.com/atom/atom/releases/download/v"
+# Fetch latest version URL for Atom Editor.
+ATOMURL=$(curl -ILs -o /dev/null -w %{url_effective} https://github.com/atom/atom/releases/latest)
+# Same as above, but regex out the version.
+ATOMLATEST=$(curl -ILs -o /dev/null -w %{url_effective} https://github.com/atom/atom/releases/latest | egrep -o '([0-9]{1,2}\.)*[0-9]{1,2}')
 # Check if Atom is installed, if yes then check if upgrade is available, if no then install.
 if [ -e /bin/atom ]
 then
-    # Download, upgrade & remove d/l file
-    # Fetch latest version URL for Atom Editor.
-    atomurl=$(curl -ILs -o /dev/null -w %{url_effective} https://github.com/atom/atom/releases/latest)
-    # Same as above, but regex out the version.
-    atomlatest=$(curl -ILs -o /dev/null -w %{url_effective} https://github.com/atom/atom/releases/latest | egrep -o '([0-9]{1,2}\.)*[0-9]{1,2}')
     # Fetch version of installed Atom Editor.
-    atominstalled=$(dnf info atom --cacheonly | grep Version | egrep -o '([0-9]{1,2}\.)*[0-9]{1,2}')
-    if [ "$atomlatest" != "$atominstalled" ]
+    ATOMINSTALLED=$(dnf info atom --cacheonly | grep Version | egrep -o '([0-9]{1,2}\.)*[0-9]{1,2}')
+    if [ "$ATOMLATEST" != "$ATOMINSTALLED" ]
     then
-        atomrpm=$(echo "atom.x86_64.rpm")
-        atomdownload=$(echo "https://github.com/atom/atom/releases/download/v")
-        wget -q --show-progress $atomdownload$atomlatest/$atomrpm -P /tmp/
-        sudo dnf -y upgrade /tmp/$atomrpm
-        rm /tmp/$atomrpm
+        # Download, upgrade & remove d/l file
+        wget -q --show-progress $ATOMDOWNLOAD$ATOMLATEST/$ATOMRPM -P /tmp/
+        sudo dnf -y upgrade /tmp/$ATOMRPM
+        rm /tmp/$ATOMRPM
     else
-        echo "You already have the latest version of Atom Editor v$atomlatest installed."
+        echo "You already have the latest version of Atom Editor v$ATOMLATEST installed."
     fi
 else
     clear
     echo "Installing Atom Editor..."
-    wget -q --show-progress $atomdownload$atomlatest/$atomrpm -P /tmp/
-    sudo dnf -y install /tmp/$atomrpm
-    rm /tmp/$atomrpm
+    wget -q --show-progress $ATOMDOWNLOAD$ATOMLATEST/$ATOMRPM -P /tmp/
+    sudo dnf -y install /tmp/$ATOMRPM
+    rm /tmp/$ATOMRPM
 fi
